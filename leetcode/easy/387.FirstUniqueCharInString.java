fcbf235a854c9ed08689ab4b1ad465d870134942
Robbie08 <08ortiz.roberto@gmail.com>
2020-10-03 23:30:57 -0700

    Included the prompt for each solution

--- leetcode/easy/387.FirstUniqueCharInString.java
+++ leetcode/easy/387.FirstUniqueCharInString.java
@@ -1,3 +1,13 @@
+// Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
+
+// Examples:
+
+// s = "leetcode"
+// return 0.
+
+// s = "loveleetcode"
+// return 2.
+
 public int firstUniqChar(String s) {
         Hashtable<Character, Integer> htable = new Hashtable<Character, Integer>(); // create our hashtable
         
