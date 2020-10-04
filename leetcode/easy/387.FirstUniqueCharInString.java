c328a6a2fbd2f8c15f88dd661a6db61d758de1ab
Robbie08 <08ortiz.roberto@gmail.com>
2020-10-03 22:58:43 -0700

    Set Up folder structure and add first file

---
+++ leetcode/easy/387.FirstUniqueCharInString.java
@@ -0,0 +1,28 @@
+public int firstUniqChar(String s) {
+        Hashtable<Character, Integer> htable = new Hashtable<Character, Integer>(); // create our hashtable
+        
+        if(s.length() == 0)
+            return -1;
+        
+        for(int i = 0; i < s.length(); i++)
+        {
+            // check if value is in hashtable
+            if(!htable.containsKey(s.charAt(i)))
+                htable.put(s.charAt(i),1); // initialize with value of 1
+            else
+                htable.put(s.charAt(i), htable.get(s.charAt(i))+1); // we must increase counter   
+        }
+    
+        int index = -1;
+        // we just need to loop through the elements again and see if the key is == 1
+        for(int j = 0; j < s.length(); j++)
+        {
+            if(htable.get(s.charAt(j)) == 1)
+            {
+                index = j; // store the current index
+                break;
+            }
+        }
+        
+        return index;
+    }
